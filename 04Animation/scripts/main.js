c1550c6912b052439bf3cbd3fbcabd19c1de2bf7
Justus <justus-bremer@gmx.de>
2020-06-21 23:43:26 +0200

    Assigment 4-7 version 15

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -12,6 +12,12 @@ function spin(){
 
   myOtherBox.object3D.position.x += move;
   myOtherBox.object3D.position.y += move;
+
+	if (myOtherBox.object3D.position.y > 10) {
+		move = -0.1;
+	} else if (myOtherBox.object3D.position.y < 0) {
+		move = 0.1;
+	}
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
