ae57c1af1e95b5142f65146ff0010ded81680f97
Justus <justus-bremer@gmx.de>
2020-06-21 21:41:50 +0200

    Assignment 4-7 version 9

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -2,21 +2,21 @@
 
 // changing individual properties with code and using setInterval
 var rotationSpeed = 0.02;
-var makeBigger = 0.01;
+var move = 0.01;
+var makeBigger = 0.1;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
 	myOtherBox.object3D.rotation.x -= rotationSpeed;
   myOtherBox.object3D.rotation.y += rotationSpeed;
 	myOtherBox.object3D.rotation.z -= rotationSpeed;
-  myOtherBox.object3D.position.z += makeBigger;
+
+  elongate();
 }
 
 function elongate() {
-
-
-
+  myOtherBox.object3D.position.z += move;
+  myOtherBox.object3D.width += makeBigger;
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
-// setInterval(elongate, 16); //equivalent to 60 fps
